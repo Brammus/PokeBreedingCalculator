2405d07f916ff1943e3e9b86bb4da39c3ed5b718
bgrootButler <50593857+Brammus@users.noreply.github.com>
2023-05-17 00:20:08 +0200

    :hamburger: added probabilities and possibilities

--- logic.py
+++ logic.py
@@ -1,11 +1,12 @@
 from random import randint
-# used for calculating possible IV outcomes for Pokemon with 2 inputs, parent 1 and parent 2 (including items)
+# used for calculating possible IV outcomes for Pokémon with 2 inputs, parent 1 and parent 2 (including items)
+# example set dict {value: x, possibilities:{parent_1: x, parent_2: x, random: 'random'}}
 
 
 def get_random_iv_to_set(ivs):
     ivs_left = []
     for iv in ivs:
-        if ivs[iv] is None:
+        if ivs[iv]['value'] is None:
             ivs_left.append(iv)
     random_stat_index = randint(0, (len(ivs_left)-1))
     random_stat = ivs_left[random_stat_index]
@@ -16,14 +17,22 @@ power_items = {'Power Weight': 'HP', 'Power Bracer': 'Attack', 'Power Belt': 'De
                'Power Band': 'Special Defence', 'Power Anklet': 'Speed'}
 
 # initialize list if IV's to get for baby
-baby_pokemon = {
+example_baby = {
     'individual_values': {
-        'HP': None,
-        'Attack': None,
-        'Defence': None,
-        'Special Attack': None,
-        'Special Defence': None,
-        'Speed': None
+        'HP': {
+            'value': None, 'possibilities': {
+                'parent_1': {
+                    'value': None, 'prob': None},
+                'parent_2': {
+                    'value': None, 'prob': None},
+                'random': 'random'
+            }
+        },
+        'Attack': {'value': None, 'possibilities': {'parent_1': {'value': None, 'prob': None}, 'parent_2': {'value': None, 'prob': None}, 'random': 'random'}},
+        'Defence': {'value': None, 'possibilities': {'parent_1': {'value': None, 'prob': None}, 'parent_2': {'value': None, 'prob': None}, 'random': 'random'}},
+        'Special Attack': {'value': None, 'possibilities': {'parent_1': {'value': None, 'prob': None}, 'parent_2': {'value': None, 'prob': None}, 'random': 'random'}},
+        'Special Defence': {'value': None, 'possibilities': {'parent_1': {'value': None, 'prob': None}, 'parent_2': {'value': None, 'prob': None}, 'random': 'random'}},
+        'Speed': {'value': None, 'possibilities': {'parent_1': {'value': None, 'prob': None}, 'parent_2': {'value': None, 'prob': None}, 'random': 'random'}},
     }
 }
 parent_1 = {
@@ -73,31 +82,63 @@ if parent_power_items > 0 and parent_power_inherit == 1:
     parent_item = parent_1['held_item']
     item_stat = power_items[parent_item]
     parent_stat = parent_1['individual_values'][item_stat]
-    baby_pokemon['individual_values'][item_stat] = parent_stat
+    parent_stat2 = parent_2['individual_values'][item_stat]
+    example_baby['individual_values'][item_stat]['value'] = parent_stat
+
+    example_baby['individual_values'][item_stat]['possibilities']['parent_1']['value'] = parent_stat
+    example_baby['individual_values'][item_stat]['possibilities']['parent_1']['prob'] = 100
+    example_baby['individual_values'][item_stat]['possibilities']['parent_2']['value'] = parent_stat2
+    example_baby['individual_values'][item_stat]['possibilities']['parent_2']['prob'] = 0
+    example_baby['individual_values'][item_stat]['possibilities']['random'] = 0
+
     ivs_to_set = ivs_to_set - 1
 elif parent_power_items > 0 and parent_power_inherit == 2:
     # inherit from parent_2
     parent_item = parent_2['held_item']
     item_stat = power_items[parent_item]
     parent_stat = parent_2['individual_values'][item_stat]
-    baby_pokemon['individual_values'][item_stat] = parent_stat
+    parent_stat1 = parent_1['individual_values'][item_stat]
+    example_baby['individual_values'][item_stat]['value'] = parent_stat
+
+    example_baby['individual_values'][item_stat]['possibilities']['parent_1']['value'] = parent_stat1
+    example_baby['individual_values'][item_stat]['possibilities']['parent_1']['prob'] = 0
+    example_baby['individual_values'][item_stat]['possibilities']['parent_2']['value'] = parent_stat
+    example_baby['individual_values'][item_stat]['possibilities']['parent_2']['prob'] = 100
+    example_baby['individual_values'][item_stat]['possibilities']['random'] = 0
+
     ivs_to_set = ivs_to_set - 1
 
 
 # inheriting IVs
 for iv in range(ivs_to_set):
     # pick random iv
-    stat_to_set = get_random_iv_to_set(baby_pokemon['individual_values'])
+    stat_to_set = get_random_iv_to_set(example_baby['individual_values'])
     random_parent = randint(1, 2)
 
     if random_parent == 1:
-        baby_pokemon['individual_values'][stat_to_set] = parent_1['individual_values'][stat_to_set]
+        example_baby['individual_values'][stat_to_set]['value'] = parent_1['individual_values'][stat_to_set]
     else:
-        baby_pokemon['individual_values'][stat_to_set] = parent_2['individual_values'][stat_to_set]
+        example_baby['individual_values'][stat_to_set]['value'] = parent_2['individual_values'][stat_to_set]
+
+    example_baby['individual_values'][stat_to_set]['possibilities']['parent_1']['value'] = parent_1['individual_values'][stat_to_set]
+    example_baby['individual_values'][stat_to_set]['possibilities']['parent_1']['prob'] = 40
+    example_baby['individual_values'][stat_to_set]['possibilities']['parent_2']['value'] = parent_2['individual_values'][stat_to_set]
+    example_baby['individual_values'][stat_to_set]['possibilities']['parent_2']['prob'] = 40
+    example_baby['individual_values'][stat_to_set]['possibilities']['random'] = 20
 
 # setting leftover IVs
-for iv in baby_pokemon['individual_values']:
-    if baby_pokemon['individual_values'][iv] is None:
-        baby_pokemon['individual_values'][iv] = randint(1, 31)
+for iv in example_baby['individual_values']:
+    if example_baby['individual_values'][iv]['value'] is None:
+        example_baby['individual_values'][iv]['value'] = randint(1, 31)
 
-print(baby_pokemon)
+        example_baby['individual_values'][iv]['possibilities']['parent_1']['value'] = parent_1['individual_values'][iv]
+        example_baby['individual_values'][iv]['possibilities']['parent_1']['prob'] = 40
+        example_baby['individual_values'][iv]['possibilities']['parent_2']['value'] = parent_2['individual_values'][iv]
+        example_baby['individual_values'][iv]['possibilities']['parent_2']['prob'] = 40
+        example_baby['individual_values'][iv]['possibilities']['random'] = 20
+
+output = f'based on your input these are the possibilities and probabilities: \n'
+for stat in example_baby['individual_values']:
+    output += f'{stat}: {example_baby["individual_values"][stat]["value"]} ({example_baby["individual_values"][stat]["possibilities"]["parent_1"]["value"]}: {example_baby["individual_values"][stat]["possibilities"]["parent_1"]["prob"]}%, {example_baby["individual_values"][stat]["possibilities"]["parent_2"]["value"]}: {example_baby["individual_values"][stat]["possibilities"]["parent_2"]["prob"]}%, random: {example_baby["individual_values"][stat]["possibilities"]["random"]}%) \n'
+
+print(output)
